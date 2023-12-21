ca56fceded39f134329e216ad05bfb3264aed4be
Marek Lotocki <mareklotocki@gmail.com>
2023-12-21 12:09:33 +0100

    formatting document

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as core from '@actions/core'
  */
 export async function run(): Promise<void> {
   try {
-    core.setOutput('state','running')
+    core.setOutput('state', 'running')
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
