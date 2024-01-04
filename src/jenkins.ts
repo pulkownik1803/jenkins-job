be9ff87e4a3087cfead2464d8a86ab61934819e2
Marek Lotocki <mareklotocki@gmail.com>
2024-01-04 14:05:14 +0100

    fix

--- src/jenkins.ts
+++ src/jenkins.ts
@@ -38,7 +38,6 @@ export async function runJenkinsJobWithParameters(url: string, crumbRequired: bo
     headers.set('Authorization', 'Basic ' + base64.encode(username + ":" + token));
     headers.append('Content-Type', 'multipart/form-data');
     const urlJob = urljoin.default(url, 'job', job, 'buildWithParameters');
-    const jobParameters = {'Repo': 'Value'};
     core.debug('Jenkins job url: ' + urlJob);
     if (crumbRequired) {
         headers.append('Jenkins-Crumb', (await getJenkinsCrumb(url, headers)).toString());
