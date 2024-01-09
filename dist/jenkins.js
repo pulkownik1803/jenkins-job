a9da0fd6cf000329d3aa7866a3916b2d7706319b
Marek Lotocki <mareklotocki@gmail.com>
2024-01-09 13:46:49 +0100

    ES2016

--- dist/jenkins.js
+++ dist/jenkins.js
@@ -22,75 +22,92 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.runJenkinsJobWithParameters = exports.runJenkinsJob = void 0;
 const core = __importStar(require("@actions/core"));
-async function getJenkinsCrumb(url, headers) {
-    const base64 = require('base-64');
-    let urljoin = await import('url-join');
-    const crumbUrl = urljoin.default(url, 'crumbIssuer', 'api', 'json');
-    core.debug('Jenkins crumb url: ' + crumbUrl);
-    return fetch(crumbUrl, {
-        method: 'GET',
-        headers: headers
-    }).then(Response => Response.json()).then(ResponseData => ResponseData.crumb);
+function getJenkinsCrumb(url, headers) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const base64 = require('base-64');
+        let urljoin = yield import('url-join');
+        const crumbUrl = urljoin.default(url, 'crumbIssuer', 'api', 'json');
+        core.debug('Jenkins crumb url: ' + crumbUrl);
+        return fetch(crumbUrl, {
+            method: 'GET',
+            headers: headers
+        }).then(Response => Response.json()).then(ResponseData => ResponseData.crumb);
+    });
 }
-async function runJenkinsJob(url, crumbRequired, job, username, token) {
-    const base64 = require('base-64');
-    const headers = new Headers();
-    let urljoin = await import('url-join');
-    headers.set('Authorization', 'Basic ' + base64.encode(username + ":" + token));
-    if (crumbRequired) {
-        headers.append('Jenkins-Crumb', (await getJenkinsCrumb(url, headers)).toString());
-    }
-    let urlJob = urljoin.default(url, 'job', job, 'build');
-    core.debug('Jenkins job url: ' + urlJob);
-    core.info(String(await getJenkinsJobParametrized(url, headers, job, true)));
-    return fetch(urlJob, {
-        method: 'POST',
-        headers: headers
-    }).then(Response => Response.statusText);
+function runJenkinsJob(url, crumbRequired, job, username, token) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const base64 = require('base-64');
+        const headers = new Headers();
+        let urljoin = yield import('url-join');
+        headers.set('Authorization', 'Basic ' + base64.encode(username + ":" + token));
+        if (crumbRequired) {
+            headers.append('Jenkins-Crumb', (yield getJenkinsCrumb(url, headers)).toString());
+        }
+        let urlJob = urljoin.default(url, 'job', job, 'build');
+        core.debug('Jenkins job url: ' + urlJob);
+        core.info(String(yield getJenkinsJobParametrized(url, headers, job, true)));
+        return fetch(urlJob, {
+            method: 'POST',
+            headers: headers
+        }).then(Response => Response.statusText);
+    });
 }
 exports.runJenkinsJob = runJenkinsJob;
-async function runJenkinsJobWithParameters(url, crumbRequired, job, username, token, parameters) {
-    const base64 = require('base-64');
-    const headers = new Headers();
-    let urljoin = await import('url-join');
-    headers.set('Authorization', 'Basic ' + base64.encode(username + ":" + token));
-    headers.append('Content-Type', 'application/x-www-form-urlencoded');
-    const urlJob = urljoin.default(url, 'job', job, 'buildWithParameters');
-    core.debug('Jenkins job url: ' + urlJob);
-    if (crumbRequired) {
-        headers.append('Jenkins-Crumb', (await getJenkinsCrumb(url, headers)).toString());
-    }
-    let httpParams = new URLSearchParams();
-    let jsonParams = JSON.parse(parameters);
-    for (let key in jsonParams) {
-        httpParams.set(key, jsonParams[key]);
-    }
-    core.info(String(await getJenkinsJobParametrized(url, headers, job, true)));
-    core.info(parameters);
-    return fetch(urlJob, {
-        method: 'POST',
-        headers: headers,
-        body: httpParams
-    }).then(Response => Response.statusText);
+function runJenkinsJobWithParameters(url, crumbRequired, job, username, token, parameters) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const base64 = require('base-64');
+        const headers = new Headers();
+        let urljoin = yield import('url-join');
+        headers.set('Authorization', 'Basic ' + base64.encode(username + ":" + token));
+        headers.append('Content-Type', 'application/x-www-form-urlencoded');
+        const urlJob = urljoin.default(url, 'job', job, 'buildWithParameters');
+        core.debug('Jenkins job url: ' + urlJob);
+        if (crumbRequired) {
+            headers.append('Jenkins-Crumb', (yield getJenkinsCrumb(url, headers)).toString());
+        }
+        let httpParams = new URLSearchParams();
+        let jsonParams = JSON.parse(parameters);
+        for (let key in jsonParams) {
+            httpParams.set(key, jsonParams[key]);
+        }
+        core.info(String(yield getJenkinsJobParametrized(url, headers, job, true)));
+        core.info(parameters);
+        return fetch(urlJob, {
+            method: 'POST',
+            headers: headers,
+            body: httpParams
+        }).then(Response => Response.statusText);
+    });
 }
 exports.runJenkinsJobWithParameters = runJenkinsJobWithParameters;
-async function getJenkinsJobParametrized(url, headers, job, crumbRequired = false) {
-    // Import required modules
-    let urljoin = await import('url-join');
-    const base64 = require('base-64');
-    // Get crumb when required
-    if (crumbRequired) {
-        headers.append('Jenkins-Crumb', (await getJenkinsCrumb(url, headers)).toString());
-    }
-    // Construct url to get details of the job 
-    const urlJob = urljoin.default(url, 'job', job, 'api/json');
-    core.debug('Jenkins job url: ' + urlJob);
-    return await fetch(urlJob, {
-        method: 'POST',
-        headers: headers,
-    }).then(Response => Response.json()).then(ResponseData => ResponseData.property.length);
+function getJenkinsJobParametrized(url, headers, job, crumbRequired = false) {
+    return __awaiter(this, void 0, void 0, function* () {
+        // Import required modules
+        let urljoin = yield import('url-join');
+        const base64 = require('base-64');
+        // Get crumb when required
+        if (crumbRequired) {
+            headers.append('Jenkins-Crumb', (yield getJenkinsCrumb(url, headers)).toString());
+        }
+        // Construct url to get details of the job 
+        const urlJob = urljoin.default(url, 'job', job, 'api/json');
+        core.debug('Jenkins job url: ' + urlJob);
+        return yield fetch(urlJob, {
+            method: 'POST',
+            headers: headers,
+        }).then(Response => Response.json()).then(ResponseData => ResponseData.property.length);
+    });
 }
 //# sourceMappingURL=jenkins.js.map
--- dist/main.js
+++ dist/main.js
@@ -22,6 +22,15 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.run = void 0;
 const core = __importStar(require("@actions/core"));
@@ -30,16 +39,18 @@ const jenkins_1 = require("./jenkins");
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
-async function run() {
-    try {
-        let status = (0, jenkins_1.runJenkinsJobWithParameters)(core.getInput('url'), (core.getInput('crumbRequired') == 'true'), core.getInput('job'), core.getInput('username'), core.getInput('token'), core.getInput('params'));
-        core.info((await status).toString());
-        core.setOutput('status', status);
-    }
-    catch (error) {
-        if (error instanceof Error)
-            core.setFailed(error.message);
-    }
+function run() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            let status = (0, jenkins_1.runJenkinsJobWithParameters)(core.getInput('url'), (core.getInput('crumbRequired') == 'true'), core.getInput('job'), core.getInput('username'), core.getInput('token'), core.getInput('params'));
+            core.info((yield status).toString());
+            core.setOutput('status', status);
+        }
+        catch (error) {
+            if (error instanceof Error)
+                core.setFailed(error.message);
+        }
+    });
 }
 exports.run = run;
 //# sourceMappingURL=main.js.map
